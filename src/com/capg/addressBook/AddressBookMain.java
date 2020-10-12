8b8de538861c950faf7ddec4207147168fadf125
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-12 18:16:44 +0530

    [NISHANT] add : Ability to edit existing contact

--- src/com/capg/addressBook/AddressBookMain.java
+++ src/com/capg/addressBook/AddressBookMain.java
@@ -6,22 +6,50 @@ public class AddressBookMain
 {
 	static Scanner sc=new Scanner(System.in);	
 	int numOfContact=0;
-	private Contact[] contactArray;
-	
+	private ArrayList<Contact> contactArray;
+	private Map<String,Contact> contactMap;
 	public AddressBookMain()
 	{
-		contactArray=new Contact[5];
+		contactArray=new ArrayList<>();
+		contactMap=new HashMap<>();
 	}
-	private void addNewContact(String firstName,String lastName,String address,String city,String state,String zip,String mobileNumber,String emailId)
+	public void addNewContact(String firstName,String lastName,String address,String city,String state,String zip,String mobileNumber,String emailId)
 	{
-		contactArray[numOfContact]=new Contact(firstName,lastName,address,city,state,zip,mobileNumber,emailId);
-		System.out.println(contactArray[numOfContact]);
-		numOfContact++;
+		Contact newContact = new Contact(firstName,lastName,address,city,state,zip,mobileNumber,emailId);
+		contactArray.add(newContact);
+		contactMap.put(firstName,newContact);
+	}
+	public void printContact()
+	{
+	System.out.println(contactArray);
+	}
+	public void editContact()
+	{
+		System.out.println("Enter The First Name to edit the contact details");
+		String fName1=sc.next();
+		Contact obj=contactMap.get(fName1);
+		System.out.println("Enter the Address");
+		String address1=sc.next();
+		obj.setAddress(address1);
+		System.out.println("Enter the City");
+		String city1=sc.next();
+		obj.setCity(city1);
+		System.out.println("Enter the State");
+		String state1=sc.next();
+		obj.setState(state1);
+		System.out.println("Enter the Zip");
+		String zip=sc.next();
+		obj.setZip(zip);
+		System.out.println("Enter the Number");
+		String number1=sc.next();
+		obj.setMobileNumber(number1);
+		System.out.println("Enter the Email");
+		String email1=sc.next();
+		obj.setEmailId(email1);
 	}
 	public static void main(String args[])
 	{
-
-	System.out.print("Enter First Name,Last Name,Address,City,State,Zip,Mobile Number,Email (In order Mentioned): ");
+	System.out.println("Enter FirstName,Last Name,Address,City,State,Zip,Mobile Number,Email ID (In mentioned order) :");
 	String fname=sc.next();
 	String lname=sc.next();
 	String newAddress=sc.next();
@@ -32,5 +60,9 @@ public class AddressBookMain
 	String email=sc.next();
 	AddressBookMain Contact1=new AddressBookMain();
 	Contact1.addNewContact(fname,lname,newAddress,newCity,newState,zip,mobileNumber,email);
+	Contact1.printContact();
+	Contact1.editContact();
+	System.out.println("Contact Details After Editing : ");
+	Contact1.printContact();
 	}
 }
--- src/com/capg/addressBook/Contact.java
+++ src/com/capg/addressBook/Contact.java
@@ -21,6 +21,71 @@ public class Contact {
 	         this.mobileNumber=mobileNumber;
 	         this.emailId=emailId;
 	        }
+	        
+		public String getFirstName() {
+				return firstName;
+			}
+
+			public void setFirstName(String firstName) {
+				this.firstName = firstName;
+			}
+
+			public String getLastName() {
+				return lastName;
+			}
+
+			public void setLastName(String lastName) {
+				this.lastName = lastName;
+			}
+
+			public String getAddress() {
+				return address;
+			}
+
+			public void setAddress(String address) {
+				this.address = address;
+			}
+
+			public String getState() {
+				return state;
+			}
+
+			public void setState(String state) {
+				this.state = state;
+			}
+
+			public String getCity() {
+				return city;
+			}
+
+			public void setCity(String city) {
+				this.city = city;
+			}
+
+			public String getZip() {
+				return zip;
+			}
+
+			public void setZip(String zip) {
+				this.zip = zip;
+			}
+
+			public String getMobileNumber() {
+				return mobileNumber;
+			}
+
+			public void setMobileNumber(String mobileNumber) {
+				this.mobileNumber = mobileNumber;
+			}
+
+			public String getEmailId() {
+				return emailId;
+			}
+
+			public void setEmailId(String emailId) {
+				this.emailId = emailId;
+			}
+
 		@Override
 		public String toString()
 	        {
