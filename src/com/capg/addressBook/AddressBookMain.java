3db6b93d04ad5294baf1692db3f91afe2664e7c6
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-12 17:17:37 +0530

    [NISHANT] add : Ability to add new contact

--- src/com/capg/addressBook/AddressBookMain.java
+++ src/com/capg/addressBook/AddressBookMain.java
@@ -1,7 +1,34 @@
 package com.capg.addressBook;
 
-public class AddressBookMain {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program");
+public class AddressBookMain
+{
+	private String firstName;
+        private String lastName;
+        private String address;
+        private String state;
+        private String city;
+        private int zip;
+        private long phoneNumber;
+        private String emailId;
+
+        public AddressBookMain(String firstName,String lastName,String address,String city,String state,int zip,long phoneNumber,String emailId)
+        {
+         this.firstName=firstName;
+         this.lastName=lastName;
+         this.address=address;
+         this.city=city;
+         this.state=state;
+         this.zip=zip;
+         this.phoneNumber=phoneNumber;
+         this.emailId=emailId;
+        }
+	@Override
+	public String toString()
+	{
+	return "First Name: "+firstName+" \nLast Name: "+lastName+"\nAddress :"+address+"\nCity: "+city+"\nZip: "+zip+"\nState: "+state+"\nPhoneNumber: "+phoneNumber+"\nEmail ID: "+emailId;
+	}
+	public static void main(String args[])
+	{
+	
 	}
 }
